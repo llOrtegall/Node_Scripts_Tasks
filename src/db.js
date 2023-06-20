fb3a00c50e6c553b343db61eb33dbbb940ff0cf0
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-20 10:30:53 -0500

    desde 0

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import {createPool}  from "mysql2/promise"
-
-export const pool = createPool({
-    host: 'localhost', 
-    user: 'root',
-    password: '123456Asd.',
-    port: 3306,
-    database: 'companydb'
-});
