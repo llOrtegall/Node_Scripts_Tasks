87cccef944cb19453353ef852dcc23147c096045
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-22 11:03:36 -0500

    agregado auto changes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ app.use(express.json()) // middleware qye trandforma la req.body a un json
 
 const PORT = 3000
 
-app.get('/ping', (req, res) => {
-    console.log('Esta funcionando ¡¡¡')
+app.get('/ping', (_req, res) => {
+    
+    console.log('Esta funcionando ¡¡¡' + new Date().toLocaleDateString())
     res.send('pong')
 })
 
 app.listen(PORT, () => {
-    console.log(`el servidor esta en el puerto${PORT}`)
+    console.log(`el servidor esta en el puerto, ${PORT}`)
 })
