5aed6ab7925dccfed44c8f901efe5c8ceac6bcc7
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-24 09:53:16 -0500

    eslint ok con config en githut

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ app.use(express.json()) // middleware qye trandforma la req.body a un json
 
 const PORT = 3000
 
-app.get('/ping',(_req,res) => {
-	console.log('Esta funcionando :D ' + new Date().toLocaleDateString())
-	res.send('pong')
+app.get('/ping', (_req, res) => {
+  console.log('Esta funcionando :D ' + new Date().toLocaleDateString())
+  res.send('pong')
 })
 
-app.use('/api/diaries',diaryRouter)
+app.use('/api/diaries', diaryRouter)
 
-app.listen(PORT,() => {
-	console.log(`el servidor esta en el puerto: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`el servidor esta en el puerto: ${PORT}`)
 })
--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -2,12 +2,12 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/',(_req,res) => {
-    res.send('Fecht all entry diaries')
+router.get('/', (_req, res) => {
+  res.send('Fecht all entry diaries')
 })
 
-router.post('/',(_req,res) => {
-    res.send('Saving a diary ¡¡¡')
+router.post('/', (_req, res) => {
+  res.send('Saving a diary ¡¡¡')
 })
 
 export default router
