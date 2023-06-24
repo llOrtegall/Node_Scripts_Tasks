eedd6885ff363695328e06840082804e0febfd0d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-24 11:34:10 -0500

    diaryServices

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,6 +1,9 @@
-import diaryDate from './diaries.json'
+import { DiaryEntry } from '../types'
+import diaryData from './diaries.json'
 
-export const getEntries = () => diaryDate
+const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
+
+export const getEntries = () => diaries
 
 export const addEntry = () => null
 
