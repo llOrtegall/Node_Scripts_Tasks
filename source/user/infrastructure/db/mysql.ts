cc048461a79d86cc3039e0dfaafe907031328879
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-23 15:46:14 -0500

    init proyect

--- source/user/infrastructure/db/mysql.ts
+++
@@ -1,9 +0,0 @@
-import mysql, { PoolOptions } from 'mysql2/promise'
-
-export const pool = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	port: parseInt(process.env.DB_PORT || '3306'),
-	database: process.env.DB_NAME,
-} as PoolOptions)
